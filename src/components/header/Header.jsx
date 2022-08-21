8934eaeea242c83b450c624655dccb25b6585817
gaurav aggarwal <aggarwalgaurav12702@gmail.com>
2022-08-22 00:24:42 +0530

    editing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import HeaderSocial from './headerSocial'
+import Photo from '../../assets/photo.png'
 const Header = () => {
   return (
     <header>
@@ -9,6 +10,9 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Gaurav Aggarwal</h1>
         <h5 className="text-light">Fullstack Developer</h5>
+        
+        <img src={Photo} className='id_image' />
+      
         <CTA />
         <HeaderSocial />
         <a href="#contact" className='scroll__down'>Scroll Down</a>
